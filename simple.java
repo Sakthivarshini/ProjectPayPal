5efad062538b23430854c2f2ea64704101bcb1c2
peramanathan007 <sundaraperama005@gmail.com>
2022-11-23 17:49:49 +0000

    first commit

---
+++ simple.java
@@ -0,0 +1,11 @@
+public class simple {
+            public static void main(String[] args) {
+            System.out.println("Hello World");
+            System.out.println("Hello mars");
+            System.out.println("Hello Jupitor"); 
+    
+
+
+           }
+            }
+            
